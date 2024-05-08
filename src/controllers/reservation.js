a57fdd2aeb3c59f49d76d7184cb887fc02a80e00
NestorSanchezS <nesaenz.69@gmail.com>
2024-05-08 09:52:46 -0500

    feat: added function in controller reservation called checkAvailable

--- src/controllers/reservation.js
+++ src/controllers/reservation.js
@@ -1,6 +1,6 @@
 const Reservation = require('../models/reservation');
 
-exports.getReservations = async (req, res, next) => {
+exports.createReservation = async (req, res) => {
     const { hotelId, roomId, startDate, endDate, numberOfPeople, accommodationType, seasonId } = req.body;
     try {
         const reservation = new Reservation({
@@ -18,5 +18,36 @@ exports.getReservations = async (req, res, next) => {
         res.json({success: true, info: 'Reservation made' });
     } catch (error) {
         res.status(500).json({ success: false, info: error.message });
+    }
+}
+
+
+exports.checkAvailability = async (req, res, next) => {
+    const { hotelId, startDate, endDate } = req.body;
+    try {
+        const reservations = await Reservation.find({
+            hotel: hotelId,
+            startDate: { $lte: endDate },
+            endDate: { $gte: startDate }
+        });
+
+        let roomsAvailable = true;
+
+        reservations.forEach(reservation => {
+            if (
+                reservation.startDate <= endDate &&
+                reservation.endDate >= startDate
+            ) {
+                roomsAvailable = false;
+            }
+        });
+
+        if (roomsAvailable) {
+            res.json({success:true, info: 'Rooms available' });
+        } else {
+            res.json({success:false, info: 'Rooms not available' });
+        }
+    } catch (error) {
+        res.status(500).json({ success:false, info: error.message });
     }
 }
